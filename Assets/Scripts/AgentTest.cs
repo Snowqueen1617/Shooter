e8e7d4a90a8b730de9e6bb8a1cbeced3444f2879
Snowqueen1617 <65308047+Snowqueen1617@users.noreply.github.com>
2022-04-27 20:29:51 -0700

    Progress AI

--- Assets/Scripts/AgentTest.cs
+++ Assets/Scripts/AgentTest.cs
@@ -5,13 +5,20 @@ using UnityEngine.AI;
 
 public class AgentTest : MonoBehaviour
 {
-    public NavMeshAgent agent;
+    private NavMeshAgent agent;
     public Transform targetTransform;
     private Animator anim;
+    public Pawn pawn;
+
+    private float timeForNextNavigationCheck;
+    [SerializeField] private float timeBetweenNavigationCkecks = 0.5f;
     
     // Start is called before the first frame update
     void Start()
     {
+        // Set my Timer
+        timeForNextNavigationCheck = Time.time + timeBetweenNavigationCkecks;
+        //Load my components
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
     }
@@ -19,24 +26,32 @@ public class AgentTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(targetTransform.position);
+        if(Time.time >= timeForNextNavigationCheck)
+        {
+            //Do check
+            agent.SetDestination(targetTransform.position);
+            // Reset timer
+            timeForNextNavigationCheck = Time.time + timeBetweenNavigationCkecks;        
+        }
+
         // Get the target velocity of the navmesh agent
         Vector3 desiredVelocity = agent.desiredVelocity;
          // Set from world direction to local
          desiredVelocity = transform.InverseTransformDirection(desiredVelocity);
 
-        // TODO: Change this to use the pawns speed - so it is exactly like the player
-        float tempSpeedDeleteMe = 3;
-        desiredVelocity = desiredVelocity.normalized * tempSpeedDeleteMe;
+        // Change this to use the pawns speed - so it is exactly like the player
+        //float tempSpeedDeleteMe = 3;
+        desiredVelocity = desiredVelocity.normalized * pawn.moveSpeed;
         // Pass this in to the animator
-        anim.SetFloat("Forward", desiredVelocity.z );
+        anim.SetFloat("Forward", desiredVelocity.z);
         anim.SetFloat("Right", desiredVelocity.x);
         // Rotate towards movement direction
         Quaternion rotationToMovementDirection = Quaternion.LookRotation(agent.desiredVelocity, Vector3.up);
         
-        // TODO: Get rotation speed from pawn
-        float maxRotateSpeedTempDELETEME = 360;
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationToMovementDirection, maxRotateSpeedTempDELETEME * Time.deltaTime);
+        // Get rotation speed from pawn
+        float maxRotateSpeed = pawn.rotateSpeed;
+        
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationToMovementDirection, maxRotateSpeed * Time.deltaTime);
     }
 
     private void OnAnimatorMove()
